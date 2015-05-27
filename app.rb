dec9eb84c2ec81191be32b30e72d0cee304c97d7
p-guerin <guerin.pierre26@gmail.com>
2015-05-27 16:32:01 +0200

    exercice code cooking pret

--- app.rb
+++ app.rb
@@ -5,25 +5,3 @@ require './environments'
 class Todo < ActiveRecord::Base
 end
 
-get "/" do
-  @todos = Todo.all
-  erb :todo
-end
-
-post "/" do
-  @todos = Todo.new(params[:todos])
-  @todos.save
-  redirect "/"
-end
-
-put "/:id" do
-  @todos = Todo.find(params[:id])
-  @todos.update(params[:todos])
-  redirect "/"
-end
-
-delete "/:id" do
-  @todos = Todo.find(params[:id])
-  @todos.destroy
-  redirect "/"
-end
